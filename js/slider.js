bdf32f7527249444d504c64878fb4a959a02a993
Uladzislau Saldatsenka <midnightroam1@gmail.com>
2022-11-10 12:45:59 +0100

    fixing merge conflicts

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Slider
 let offset = 0; // offset from left angle
 const sliderLine = document.querySelector('.slider__line');
@@ -18,25 +17,4 @@ document.querySelector('.slider__prev').addEventListener('click', () => {
         offset = 100.5;
     }
     sliderLine.style.right = offset + '%';
-=======
-// Slider
-let offset = 0; // offset from left angle
-const sliderLine = document.querySelector('.slider__line');
-
-
-document.querySelector('.slider__next').addEventListener('click', () => {
-    offset += 100;
-    if (offset > 110) {
-        offset = 0;
-    }
-    sliderLine.style.right = offset + '%';
-});
-
-document.querySelector('.slider__prev').addEventListener('click', () => {
-    offset -= 100.5;
-    if (offset < 0) {
-        offset = 100.5;
-    }
-    sliderLine.style.right = offset + '%';
->>>>>>> 182bbed2ae068f4dfd6b41fd69678554cf5fbc29
 });
