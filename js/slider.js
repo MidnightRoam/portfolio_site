ea39466bbaa79acbcf60172eb2ced0960b564df8
Uladzislau Saldatsenka <midnightroam1@gmail.com>
2022-11-15 16:53:56 +0100

    Slider were rewritten for make him adaptive

--- js/slider.js
+++ js/slider.js
@@ -1,20 +1,38 @@
 // Slider
-let offset = 0; // offset from left angle
+const images = document.querySelectorAll('.slider .slider__line img')
 const sliderLine = document.querySelector('.slider__line');
+let count = 0;
+let width;
 
+function init() {
+    width = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = width * images.length +'px';
+    images.forEach(item => {
+        item.style.width = width +'px';
+        item.style.height = 'auto';
+    });
+    rollSlider();
+}
+
+window.addEventListener('resize', init); 
+init();
 
 document.querySelector('.slider__next').addEventListener('click', () => {
-    offset += 100;
-    if (offset > 110) {
-        offset = 0;
+    count++;
+    if (count >= images.length) {
+        count = 0;
     }
-    sliderLine.style.right = offset + '%';
-});
+    rollSlider();
+})
 
 document.querySelector('.slider__prev').addEventListener('click', () => {
-    offset -= 100.5;
-    if (offset < 0) {
-        offset = 100.5;
+    count--;
+    if (count < 0) {
+        count = images.length - 1;
     }
-    sliderLine.style.right = offset + '%';
-});
+    rollSlider();
+})
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-'+ count * width + 'px)';
+}
